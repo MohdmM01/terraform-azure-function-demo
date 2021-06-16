d677096d4ee27c708d0701a2c57198bb810684aa
Mohd Anis Mohd Mokhtar <mohdanis.mohdmokhtar@mattheyasia.com>
2021-06-16 18:07:28 +0800

    Update GetName.cs
    Added new function.

--- DemoFunction/GetName.cs
+++ DemoFunction/GetName.cs
@@ -14,11 +14,32 @@ namespace GetName
     public static class GetName
     {
         [FunctionName("GetName")]
-        public static async Task<IActionResult> Run(
+        public static async Task<IActionResult> GetNameById(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "getName/{userId}")] HttpRequest req,
             ILogger log, int userId)
         {
             if (userId == 1)
+            {
+                User response = new User()
+                {
+                    Id = 1,
+                    FirstName = "Alice",
+                    LastName = "Smith",
+                    Email = "alice.smith@gmail.com"
+                };
+
+                return new OkObjectResult(response);
+            }
+            else
+                return new BadRequestResult();
+        }
+
+        [FunctionName("GetNameByEmail")]
+        public static async Task<IActionResult> GetNameByEmail(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "getName/{email}")] HttpRequest req,
+            ILogger log, string email)
+        {
+            if (email == "alice.smith @gmail.com")
             {
                 User response = new User()
                 {
